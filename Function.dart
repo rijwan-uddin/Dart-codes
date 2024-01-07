a7301a75522083aef52cc9839c0b659585b42f32
Md Rijwan Uddin Sarker <96045123+rijwan-uddin@users.noreply.github.com>
2024-01-08 00:22:13 +0600

    Add files via upload

---
+++ Function.dart
@@ -0,0 +1,12 @@
+void main() {
+  add(8, 9);
+  sub(8, 9.23);
+}
+
+void add(int x, int y) {
+  print(x + y);
+}
+
+void sub(int x, double y) {
+  print(x - y);
+}
---
+++ nullSafety2.dart
@@ -0,0 +1,8 @@
+void main() {
+  int x = 10;
+  if (x == null) {
+    print('It is empty');
+  } else {
+    print("It is not empty");
+  }
+}
